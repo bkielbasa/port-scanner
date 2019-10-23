fe7f504490afbd48f0bed75f60799a7ddcde7b45
Bartłomiej Klimczak <bartlomiej.klimczak88@gmail.com>
2019-10-23 22:38:49 +0200

    fix problem with race condition

--- main.go
+++ main.go
@@ -23,18 +23,21 @@ func main() {
 	hostname := flag.String("hostname", "", "hostname to test")
 	startPort := flag.Int("start-port", 80, "the port on which the scanning starts")
 	endPort := flag.Int("end-port", 100, "the port from which the scanning ends")
-	timeout := flag.Duration("timeout", time.Millisecond * 200, "timeout")
+	timeout := flag.Duration("timeout", time.Millisecond*200, "timeout")
 	flag.Parse()
 
 	ports := []int{}
 
 	wg := &sync.WaitGroup{}
+	mutex := &sync.Mutex{}
 	for port := *startPort; port <= *endPort; port++ {
 		wg.Add(1)
 		go func(p int) {
 			opened := isOpen(*hostname, p, *timeout)
 			if opened {
+				mutex.Lock()
 				ports = append(ports, p)
+				mutex.Unlock()
 			}
 			wg.Done()
 		}(port)
