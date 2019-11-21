77a20bcb29cbf25dbde6b2838c3336bf048cb0bc
Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>
2019-11-21 21:30:50 +0100

    1 millisecond

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 )
 
 func isOpen(host string, port int, timeout time.Duration) bool {
-	time.Sleep(time.Millisecond * 1)
 	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", host, port), timeout)
 	if err == nil {
 		_ = conn.Close()
